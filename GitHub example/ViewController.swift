6eac0c4f5308cf87361cd99302ed1886c783bf0b
Ben Warren <s017274@students.lmsd.org>
2020-01-31 13:51:11 -0500

    Initial Commit

---
+++ GitHub example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub example
+//
+//  Created by Benjamin Warren (student LM) on 1/31/20.
+//  Copyright © 2020 Benjamin Warren (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
